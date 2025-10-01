3065e4a6dfbaf08eba5e1d3722c5ab81575a53f8
camilagrabowski-lgtm <camila.grabowski@aluno.senai.br>
2025-10-01 16:59:06 -0300

    Add files via upload

---
+++ Calculos/logica.js
@@ -0,0 +1,11 @@
+function soma(){
+    var numero1 = document.getElementByld("n1").valueAsNumber
+    var numero2 = document.getElementByld("n2").valueAsNumber
+    var resposta =  document.getElementByld("respSoma")
+
+    resposta.textContent = numero1 - numero2
+
+    // console.log (numero1, numero2)
+
+    
+}
