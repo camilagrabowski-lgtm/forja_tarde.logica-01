cc248b19097fedec579a3fc26d6436e948f04f8b
camilagrabowski-lgtm <camila.grabowski@aluno.senai.br>
2025-10-13 15:44:47 -0300

    Add files via upload

---
+++ IMC/Print/MeuPet/script.js
@@ -0,0 +1,7 @@
+function respostas(){
+    var nome = (document.getElementByld(nome)).value
+    var idade = parseFloat(document.getElementByld(idade).value)
+    var qual = (document.getElementByld(qual)).value
+    var raca = (document.getElementByld(raca)).value
+    var nacionalidade = (document.getElementByld(nacionalidade)).value
+}
---
+++ IMC/Print/script.js
@@ -0,0 +1,15 @@
+function escreva(){
+    var nome = document.getElementByld(nome).value
+    var idade = parseFloat(document.getElementByld(idade).value)
+
+    console.log(typeof nome, typeof idade)
+
+    var printNome = document.getElementById("printNome")
+    var printIdade = document.getElementById("printIdade")
+
+    printNome.textContent = nome
+    printNome.style.color = "green"
+
+    printIdade.textContent = idade
+    printIdade.style.color = "red"
+}
